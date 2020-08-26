3867ac7e233f9190778721171c26836bf869cd4d
jruipinto <44852805+jruipinto@users.noreply.github.com>
2020-08-26 09:15:51 +0100

    refactor: extract config and FilesService
    extract config and FilesService into their own separate files

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ const express = require('@feathersjs/express');
 const socketio = require('@feathersjs/socketio');
 const multer = require("multer");
 const path = require('path');
-const fs = require('fs');
+const FilesService = require('./files-service')
 
-const port = 3030;
-const uploadsDir = './uploads/'
+const { port, uploadsDir } = require('./config');
 const storage = multer.diskStorage({
     destination: uploadsDir,
     filename: function (req, file, cb) {
@@ -14,36 +13,6 @@ const storage = multer.diskStorage({
     },
 });
 const upload = multer({ storage });
-
-
-
-
-// A messages service that allows to create new
-// and return all existing messages
-class FileService {
-    constructor() {
-    }
-
-    async find() {
-        fs.readdir(uploadsDir, (err, files) => {
-            if (err) {
-                console.log('Unable to scan directory: ' + err);
-            }
-            files.forEach(console.log);
-            return files
-        });
-    }
-
-    async create(data) {
-        // you use this method to save in DB but as the purpose of the app is
-        // to be an upload server, you already used multer to save files before
-        // this Service in app.use('files', multer.any(), new FileService)
-        return data;
-    }
-}
-
-
-
 
 
 // Creates an ExpressJS compatible Feathers application
@@ -62,7 +31,7 @@ app.configure(socketio());
 // Register an in-memory messages service
 app.use('/files',
     upload.any(),
-    new FileService()
+    new FilesService()
 );
 // Register a nicer error handler than the default Express one
 app.use(express.errorHandler());
@@ -78,22 +47,3 @@ app.publish(data => app.channel('everybody'));
 app.listen(port).on('listening', () =>
     console.log('Feathers server listening on localhost:3030')
 );
-
-// app.post("/upload", upload.any(), function (req, res, next) {
-//     console.log(req.files || req.file);
-//     if (req.files || req.file) {
-//         return res.status(200).json(req.files || req.file);
-//     }
-//     return res.status(400).json({ msg: "PLEASE UPLOAD FILE" });
-// });
-
-// app.get('/', (req, res, next) => {
-//     return res.sendFile(path.join(__dirname, 'public/index.html'));
-// })
-
-// app.listen(
-//     port,
-//     () => {
-//         console.log('Listening on http://localhost:' + port)
-//     }
-// );
